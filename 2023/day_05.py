e4a24d2a69ac1b598d0ace2247f63115a884a446
WiseRowlet <darygh@gmail.com>
2025-01-21 11:00:17 -0700

    Update day_05.py

--- 2023/day_05.py
+++ 2023/day_05.py
@@ -1,61 +1,70 @@
+import re
+
 from aocd import get_puzzle
 
 DAY = 5
 YEAR = 2023
 
 puzzle = get_puzzle(day=DAY, year=YEAR)
-puzzle_data = puzzle.input_data
-examples = puzzle.examples
-example_input = examples[0].input_data
-part_a_example_solution = examples[0].answer_a
-part_b_example_solution = examples[0].answer_b
+puzzle_input = puzzle.input_data
 
 
-def parse_input(input_data):
-    plots = []
-    seeds = []
-    lines = input_data.splitlines()
-    seeds = list(map(int, lines[0].split(": ")[1].split(" ")))
-    plot = []
-    for line in lines[1:]:
-        if line == "":
-            continue
-        if line[0].isalpha():
-            if len(plot) > 0:
-                plots.append(plot)
-                plot = []
-        else:
-            plot.append(list(map(int, line.split(" "))))
-    plots.append(plot)
+def part1(puzzle_input):
+    segments = puzzle_input.split("\n\n")
+    seeds = re.findall(r"\d+", segments[0])
 
-    return seeds, plots
-
-
-def part_a(seeds, plots):
-    locations = []
-    for seed in seeds:
-        location = seed
-        for plot in plots:
-            for tran in plot:
-                dest, src, rng = tran[0], tran[1], tran[2]
-                if src <= location < src + rng:
-                    location = dest + location - src
+    min_location = float("inf")
+    for x in map(int, seeds):
+        for seg in segments[1:]:
+            for conversion in re.findall(r"(\d+) (\d+) (\d+)", seg):
+                destination, start, delta = map(int, conversion)
+                if x in range(start, start + delta):
+                    x += destination - start
                     break
-        locations.append(location)
-    return min(locations)
+
+        min_location = min(x, min_location)
+
+    return min_location
 
 
-def part_b(seeds, plots):
-    new_seeds = set()
-    i = 0
-    while i + 1 < len(seeds):
-        for j in range(seeds[i], seeds[i] + seeds[i + 1]):
-            new_seeds.add(j)
-        i += 2
-    # return part_a(new_seeds, plots)
-    return len(new_seeds)
+def part2(puzzle_input):
+    segments = puzzle_input.split("\n\n")
+    intervals = []
+
+    for seed in re.findall(r"(\d+) (\d+)", segments[0]):
+        x1, dx = map(int, seed)
+        x2 = x1 + dx
+        intervals.append((x1, x2, 1))
+
+    min_location = float("inf")
+    while intervals:
+        x1, x2, level = intervals.pop()
+        if level == 8:
+            min_location = min(x1, min_location)
+            continue
+
+        for conversion in re.findall(r"(\d+) (\d+) (\d+)", segments[level]):
+            z, y1, dy = map(int, conversion)
+            y2 = y1 + dy
+            diff = z - y1
+            if x2 <= y1 or y2 <= x1:  # no overlap
+                continue
+            if x1 < y1:  # split original interval at y1
+                intervals.append((x1, y1, level))
+                x1 = y1
+            if y2 < x2:  # split original interval at y2
+                intervals.append((y2, x2, level))
+                x2 = y2
+            intervals.append(
+                (x1 + diff, x2 + diff, level + 1)
+            )  # perfect overlap -> make conversion and let pass to next nevel
+            break
+
+        else:
+            intervals.append((x1, x2, level + 1))
+
+    return min_location
 
 
-seeds, plots = parse_input(puzzle_data)
-print(part_a(seeds, plots))
-print(part_b(seeds, plots))
+print("Part 1:", part1(puzzle_input))
+print("Part 2:", part2(puzzle_input))
